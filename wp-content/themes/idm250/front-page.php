a7048d8b87d07b53956b8963a72bdac3c0346471
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-07 00:58:41 -0500

    update style on front-page.php

--- wp-content/themes/idm250/front-page.php
+++ wp-content/themes/idm250/front-page.php
@@ -1,12 +1,11 @@
 <?php get_header() ?>
 
-<main class="black-background">
+<main class="black-background front-page">
   this is front-page.php
 
   <?php while (have_posts()) : the_post(); ?>
     <div class="">
-      <h1 class=""><?php the_title(); ?>
-      </h1>
+
 
       <div class="">
         <!-- start content -->
