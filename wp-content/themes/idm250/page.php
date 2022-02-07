d42fe9eebb0fa32e42488be10215cc5a0dd8b5a4
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-06 21:27:44 -0500

    updated wp content and added page.php and templates

--- wp-content/themes/idm250/page.php
+++ wp-content/themes/idm250/page.php
@@ -1,17 +1,11 @@
-<?php
-/* Template Name: Portfolio Listing */
-?>
-
-//this template can be used in the wp in the template section
-
 <?php get_header(); ?>
-
-This is template-portfolio-listing.php
 
 <?php while (have_posts()) : the_post(); ?>
 <div class="">
   <h1 class=""><?php the_title(); ?>
   </h1>
+
+  <?php the_post_thumbnail(); ?>
 
   <div class="">
     <!-- start content -->
---
+++ wp-content/themes/idm250/template-search.php
@@ -0,0 +1,11 @@
+<?php
+/* Template Name: Search */
+?>
+
+//this template can be used in the wp in the template section
+
+<?php get_header(); ?>
+
+This is template-search.php
+
+<?php get_footer();
