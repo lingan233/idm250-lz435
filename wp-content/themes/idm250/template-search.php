6936d0598ca47c6c4c536b5e376ad618d40c4b36
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-02-07 00:16:25 -0500

    add wp content

--- wp-content/themes/idm250/template-search.php
+++ wp-content/themes/idm250/template-search.php
@@ -2,12 +2,25 @@
 /* Template Name: Search */
 ?>
 
-//this template can be used in the wp in the template section
+<!-- //this template can be used in the wp in the template section -->
 
 <?php get_header(); ?>
 
 <main>
     This is template-search.php
+
+    <?php while (have_posts()) : the_post(); ?>
+    <div class="">
+      <h1 class=""><?php the_title(); ?>
+      </h1>
+
+      <div class="">
+        <!-- start content -->
+        <?php the_content(); ?>
+        <!-- end content -->
+      </div>
+    </div>
+  <?php endwhile; ?>
 </main>
 
 
