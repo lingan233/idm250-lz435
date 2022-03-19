45b5c573fefe7b88a34358e3720d42feb1ea4b3e
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-03-19 13:09:27 -0400

    archive and search update

--- wp-content/themes/idm250/functions.php
+++ wp-content/themes/idm250/functions.php
@@ -14,3 +14,12 @@ require get_template_directory() . '/includes/setup.php';
  * Custom Post Types
  */
 require get_template_directory() . '/includes/post-types.php';
+
+/**
+ * Custom Taxonomies
+ */
+require get_template_directory() . '/includes/taxonomies.php';
+/**
+ * Helper functions
+ */
+require get_template_directory() . '/includes/helpers.php';
